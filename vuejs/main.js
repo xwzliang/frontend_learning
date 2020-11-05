b531635fe740fe4830952bf80ab756be0e318c98
broliang <18706703437@163.com>
2020-11-05 19:52:40 +0800

    New: vuejs.

---
+++ vuejs/main.js
@@ -0,0 +1,31 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        inventory: 8,
+        isVisible: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+            }
+        ],
+        cart: 0,
+    },
+    methods: {
+        addToCart: function () {
+            this.cart += 1
+        },
+        updateProductImage(variantImage) {
+            this.image = variantImage
+        }
+    }
+})
